221de768325cd29c1bf2551122b341ab91cdb4cd
Romanenko Ivan <ivan.romanenko7415@gmail.com>
2019-09-25 00:03:49 +0300

    build

--- app/utils/canvas-setup.js
+++
@@ -1,9 +0,0 @@
-export const setup = (id, w, h) => {
-    const c = document.getElementById(id);
-    const ctx = c.getContext('2d');
-
-    c.width = window.innerWidth * w;
-    c.height = window.innerHeight * h;
-
-    return { c, ctx };
-};
